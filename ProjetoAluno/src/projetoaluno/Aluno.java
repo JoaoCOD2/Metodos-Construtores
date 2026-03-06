45e85d8c11248e75adec7e281fad0ab34c7910bd
JoaoCOD2 <joao.github23@gmail.com>
2026-03-05 22:51:48 -0300

    att

--- ProjetoAluno/src/projetoaluno/Aluno.java
+++ ProjetoAluno/src/projetoaluno/Aluno.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projetoaluno;
 
-/**
- *
- * @author aluno.saolucas
- */
 public class Aluno {
-    
+
+    private String nome;
+    private double nota1;
+    private double nota2;
+
+    public Aluno(String nome, double nota1, double nota2) {
+        this.nome = nome;
+        setNota1(nota1);
+        setNota2(nota2);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null && !nome.isEmpty()) {
+            this.nome = nome;
+        }
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        if (nota1 >= 0 && nota1 <= 10) {
+            this.nota1 = nota1;
+        } else {
+            System.out.println("Nota inválida.");
+        }
+    }
+
+    public void setNota2(double nota2) {
+        if (nota2 >= 0 && nota2 <= 10) {
+            this.nota2 = nota2;
+        } else {
+            System.out.println("Nota inválida.");
+        }
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public double calcularMedia() {
+        return (nota1 + nota2) / 2;
+    }
+
+    public void verificarAprovacao() {
+        if (calcularMedia() >= 6) {
+
+            System.out.println("Aluno aprovado!");
+
+        } else {
+            System.out.println("Aluno reprovado.");
+        }
+    }
 }
--- ProjetoAluno/src/projetoaluno/ProjetoAluno.java
+++ ProjetoAluno/src/projetoaluno/ProjetoAluno.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projetoaluno;
 
-/**
- *
- * @author aluno.saolucas
- */
 public class ProjetoAluno {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws InterruptedException {
+
+        Aluno a1 = new Aluno("João", 7.3, 8.2);
+
+        System.out.println("Aluno: " + a1.getNome());
+        Thread.sleep(1500);
+        System.out.println("Nota 1: " + a1.getNota1());
+        Thread.sleep(1500);
+        System.out.println("Nota 2: " + a1.getNota2());
+        Thread.sleep(1500);
+        System.out.println("Média: " + a1.calcularMedia());
+        Thread.sleep(1500);
+        a1.verificarAprovacao();
+        
+        System.out.println("-------------------------");
+
+        Aluno a2 = new Aluno("Alice", 4.5, 2.4);
+
+        Thread.sleep(1500);
+
+        System.out.println("Aluno: " + a2.getNome());
+        Thread.sleep(1500);
+        System.out.println("Nota 2: " + a2.getNota1());
+        Thread.sleep(1500);
+        System.out.println("Nota 2: " + a2.getNota2());
+        Thread.sleep(1500);
+        System.out.println("Média: " + a2.calcularMedia());
+        Thread.sleep(1500);
+        a2.verificarAprovacao();
+
     }
-    
+
 }
